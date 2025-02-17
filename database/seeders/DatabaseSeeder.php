a261afd79792e6bb5a83ace835712acc07e7b99d
E33-a <emax03736@gmail.com>
2025-02-17 03:19:15 -0600

    Initial configurations

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Role;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,11 +14,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        // Create 10 Roles
+        $roles = Role::factory(10)->create();
+
+        // Create 10 Users and asssing a random roles to each user
+        User::factory(10)->create()->each(function ($user) use ($roles) {
+            //Asssing 1 to 3 roles to each user
+            $user->roles()->attach(
+                $roles->random(rand(1, 3))->pluck('id')->toArray()
+            );
+        });
     }
 }
